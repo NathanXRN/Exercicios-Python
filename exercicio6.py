1658fb4429c488abac493e4cfa44b2f965678e5b
Nathan Nery <nathannery84@gmail.com>
2025-08-28 18:23:54 -0300

    Exercicios

---
+++ exercicio6.py
@@ -0,0 +1,8 @@
+# Escreva um programa em Python para exibir a primeira e a última cores da lista a seguir.
+
+color_list = ["Azul", "Rosa", "Verde", "Branco", "Roxo"]
+
+primeira_cor = color_list[0]
+ultima_cor = color_list[-1]
+
+print(f"A primeira cor é '{primeira_cor}' e a última cor é '{ultima_cor}'")
