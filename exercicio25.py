b21e42f0438fbbe2f3aab25b17a923bd59ed20a8
Nathan Xavier <nathannery84@gmail.com>
2025-08-31 22:01:36 -0300

    Exercicio 25

--- exercicio25.py
+++ exercicio25.py
@@ -1,10 +1,11 @@
 # Escreva um programa em Python para obter a soma de um inteiro não negativo usando recursão.
 
-def digitos_soma(num):
+def sumDigits(num):
 
-    if len(num) == 1:
-        return num[0]
+    if num == 0:
+        return 0 
     else:
-        return num[0] + digitos_soma(num[1:])
-    
-print(digitos_soma(345))
+        return num % 10 + sumDigits(int(num / 10))
+
+print(sumDigits(345))
+print(sumDigits(45))
